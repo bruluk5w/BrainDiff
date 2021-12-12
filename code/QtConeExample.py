cdd291a716869acf9d8110b78dcfa2c445dcc7d6
Lukas Brunner <bruluk5w@gmail.com>
2021-12-12 16:38:17 +0100

    Sample layout

--- code/QtConeExample.py
+++ code/QtConeExample.py
@@ -13,7 +13,7 @@ QVTKRenderWindowInteractor = QVTK.QVTKRenderWindowInteractor
 
 if QVTK.PyQtImpl == 'PySide6':
     from PySide6.QtCore import Qt
-    from PySide6.QtWidgets import QApplication, QMainWindow
+    from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget
 elif QVTK.PyQtImpl == 'PySide2':
     from PySide2.QtCore import Qt
     from PySide2.QtWidgets import QApplication, QMainWindow
@@ -22,38 +22,51 @@ else:
     from PySide.QtGui import QApplication, QMainWindow
 
 
+class SampleWidget(QWidget):
+    def __init__(self, app):
+        super().__init__()
+        self.vertical_layout = QVBoxLayout(self)
+        self.button = QPushButton(text="Push Me", )
+        self.button.clicked.connect(lambda: print("hello world"))
+        self.vertical_layout.addWidget(self.button)
+        self.renderWidget = QVTKRenderWindowInteractor()
+        self.vertical_layout.addWidget(self.renderWidget)
+
+        # if you don't want the 'q' key to exit comment this.
+        self.renderWidget.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
+
+        self.ren = vtkRenderer()
+        self.renderWidget.GetRenderWindow().AddRenderer(self.ren)
+
+        self.cone = vtkConeSource()
+        self.cone.SetResolution(8)
+
+        self.coneMapper = vtkPolyDataMapper()
+        self.coneMapper.SetInputConnection(self.cone.GetOutputPort())
+
+        self.coneActor = vtkActor()
+        self.coneActor.SetMapper(self.coneMapper)
+
+        self.ren.AddActor(self.coneActor)
+
+        # show the widget
+        #window.show()
+
+        self.renderWidget.Initialize()
+        self.renderWidget.Start()
+
+
 def QVTKRenderWidgetConeExample(argv):
     """A simple example that uses the QVTKRenderWindowInteractor class."""
     # every QT app needs an app
     app = QApplication(['QVTKRenderWindowInteractor'])
-
     window = QMainWindow()
-
-    # create the widget
-    widget = QVTKRenderWindowInteractor(window)
-    window.setCentralWidget(widget)
-    # if you don't want the 'q' key to exit comment this.
-    widget.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
-
-    ren = vtkRenderer()
-    widget.GetRenderWindow().AddRenderer(ren)
-
-    cone = vtkConeSource()
-    cone.SetResolution(8)
-
-    coneMapper = vtkPolyDataMapper()
-    coneMapper.SetInputConnection(cone.GetOutputPort())
-
-    coneActor = vtkActor()
-    coneActor.SetMapper(coneMapper)
-
-    ren.AddActor(coneActor)
-
-    # show the widget
+    ourAppWidget = SampleWidget(app)
+    window.setCentralWidget(ourAppWidget)
     window.show()
 
-    widget.Initialize()
-    widget.Start()
+    # create the widget
+
 
     # start event processing
     # Source: https://doc.qt.io/qtforpython/porting_from2.html
@@ -63,6 +76,7 @@ def QVTKRenderWidgetConeExample(argv):
         app.exec()
     except AttributeError:
         app.exec_()
+
 
 if __name__ == "__main__":
     QVTKRenderWidgetConeExample(sys.argv)
